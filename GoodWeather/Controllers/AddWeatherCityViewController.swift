a5f3c9ccb51cbab5fcbaffc03db75615b98768df
Ana Almeida <argsalmeida@gmail.com>
2020-07-26 12:17:33 +0100

    Implementing Web Services Layer

--- GoodWeather/Controllers/AddWeatherCityViewController.swift
+++ GoodWeather/Controllers/AddWeatherCityViewController.swift
@@ -16,7 +16,17 @@ class AddWeatherCityViewController: UIViewController {
     
     @IBAction func saveCityButtonPressed() {
         
-        
+        if let city = cityNameTextField.text {
+            let weatherURL = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(city)&appid=fba8e937627ce079de5d4de685252c46&units=imperial")!
+            
+            let weatherResource = Resource<Any>(url: weatherURL) { data -> Any? in
+                return data
+            }
+            Webservice().load(resource: weatherResource) { result in
+                
+            }
+            
+        }
         
     }
     
---
+++ GoodWeather/Services/Webservice.swift
@@ -0,0 +1,33 @@
+//
+//  Webservice.swift
+//  GoodWeather
+//
+//  Created by Ana Almeida on 26/07/2020.
+//  Copyright © 2020 Ana Almeida. All rights reserved.
+//
+
+import Foundation
+
+struct Resource<T> {
+    let url: URL
+    let parse: (Data) -> T?
+}
+
+final class Webservice {
+    
+    func load<T>(resource: Resource<T>, completion: @escaping (T?) -> ()){
+        
+        URLSession.shared.dataTask(with: resource.url) { (data, respone, error) in
+            
+            print(data)
+            
+            if let data = data {
+                DispatchQueue.main.async {
+                    completion(resource.parse(data))
+                }
+            } else {
+                completion(nil)
+            }
+        }.resume()
+    }
+}
